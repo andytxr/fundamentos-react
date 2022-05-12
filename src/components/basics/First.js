ac464f6abee3cf94abf298f9c69b24740fdd34ea
andytxr <andreluiiz.gst@gmail.com>
2022-05-12 15:33:17 -0300

    Implementando primeiro componente

---
+++ src/components/basics/First.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function First(){
+    const msg = 'Welcome!'
+    return (
+        <div>
+            <h2>
+                First Component
+            </h2>
+
+            <p>   
+                {msg}
+            </p>
+        </div>);
+
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import './index.css';
+import First from './components/basics/First.js'
+
+ReactDOM.render(<div><First></First></div>, document.getElementById('root'));
